fe1e7b0cb11b61aad53daab9a48e114efae35fc1
Nathan14B <nathanadnerbueno2002@gmail.com>
2022-04-06 14:25:45 -0300

    Class Date implemented

--- src/application/DateTest.java
+++ src/application/DateTest.java
@@ -1,10 +1,12 @@
 package application;
 
+import model.entities.Date;
+
 public class DateTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Date birthday = new Date(26, 8, 1974);
+		System.out.println(birthday);
 	}
 
 }
--- src/model/entities/Date.java
+++ src/model/entities/Date.java
@@ -1,5 +1,48 @@
 package model.entities;
 
 public class Date {
-
+	private byte day;
+	private byte month;
+	private short year;
+	
+	public Date(byte day, byte month, short year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public Date(int day, int month, int year) {
+		this.day = (byte) day;
+		this.month = (byte) month;
+		this.year = (short) year;
+	}
+	
+	public byte getDay() {
+		return day;
+	}
+	
+	public void setDay(byte day) {
+		this.day = day;
+	}
+	
+	public byte getMonth() {
+		return month;
+	}
+	
+	public void setMonth(byte month) {
+		this.month = month;
+	}
+	
+	public short getYear() {
+		return year;
+	}
+	
+	public void setYear(short year) {
+		this.year = year;
+	}
+	
+	@Override
+	public String toString() {
+		return day + "/" + month + "/" + year;
+	}
 }
